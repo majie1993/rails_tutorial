3fb222cd4107247cfa42ea96a568b8a3497c0e70
WaterWood <majieqq@gmail.com>
2013-11-21 12:44:16 +0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name    "Majie"
+        email   "majieqq@gmail.com"
+        password "852845"
+        password_confirmation "852845"
+    end
+end
