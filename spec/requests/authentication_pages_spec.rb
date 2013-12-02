2df1acef63b4d6ff95e7e2136498bcc45b72ad5c
WaterWood <majieqq@gmail.com>
2013-12-02 21:52:08 +0800

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -83,6 +83,15 @@ describe "Authentication" do
                     end
                 end
             end
+
+            describe "in the Microposts controller" do
+                before { post microposts_path }
+                specify { expect(response).to redirect_to(signin_path) }
+            end
+            describe "submitting to the destroy action" do
+                before { delete micropost_path(FactoryGirl.create(:micropost)) }
+                specify { expect(response).to redirect_to(signin_path) }
+            end
         end
 
         describe "as wrong user" do
